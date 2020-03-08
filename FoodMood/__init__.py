bed7d56c46715185d6eec2038f3c0d735af61d67
shafali731 <shafaligupta31@gmail.com>
2020-03-08 05:17:41 -0400

    deployed

--- FoodMood/__init__.py
+++ FoodMood/__init__.py
@@ -39,50 +39,6 @@ def welcome(term,place,price):
         resturs_img[k]= y[str(k)]['image_url']
     print(lats)
     return render_template('index.html', imgs= resturs_img, resturs_urls=resturs_urls, resturs_names=resturs_names,ret =y, lats=lats, longs= longs, amount = int(amount), term = term, place = place, price = price)
-    # @app.route('/result/<term>/<place>')
-    # def welcome(term,place):
-    #     ret = sample.query_api(term,place)
-    #     y = json.loads(ret)
-    #     t = list(y.values())
-    #     amount = t[-1]
-    #     lats = ["" for p in range(int(amount))]
-    #     longs = ["" for p in range(int(amount))]
-    #     resturs_urls = ["" for x in range(int(amount))]
-    #     resturs_names = ["" for x in range(int(amount))]
-    #     resturs_img= ["" for x in range(int(amount))]
-    #     # locations = []
-    #     # print(locations)
-    #     for k in range(int(amount)):
-    #         lats[k]= y[str(k)]["coordinates"]["latitude"]
-    #         longs[k] = y[str(k)]["coordinates"]["longitude"]
-    #         # resturs[k]= y[str(k)]['name']
-    #         # print(locations[k])
-    #     # print(t[0])
-    #     amount_double = (int(amount))
-    #     u =0
-    #     # while(u <amount_double):
-    #     #     resturs_urls[u]= y[str(u)]['url']
-    #     #     u+=1
-    #     # print("u:  ")
-    #     # print(u)
-    #     # print(amount_double*2)
-    #     for k in range(int(amount)):
-    #         resturs_names[k]= y[str(k)]['name']
-    #         resturs_urls[k]= y[str(k)]['url']
-    #         resturs_img[k]= y[str(k)]['image_url']
-    #     # while(u<(amount_double*2)):
-    #     #     resturs[u]= y[str(u)]['name']
-    #     #     u+=1
-    #         # resturs[u+1]=y[str(u)]
-    #         # u+=2
-    #     # print(locations)
-    #     # print(lats)
-    #     # print(y)
-    #     # print(y["1"]['name'])
-    #     print(resturs_names)
-    #     # print(amount)
-    #     return render_template('index.html', imgs= resturs_img, resturs_urls=resturs_urls, resturs_names=resturs_names,ret =y, lats=lats, longs= longs, amount = int(amount))
-# @app.route('/result/<term>/<place>/<check_one>/<check_two>/<check_three>/<check_four>')
 
 @app.route('/sort_price', methods=["GET"])
 def results_price():
@@ -90,8 +46,6 @@ def results_price():
     loc = request.args.get('place')
     print("term:" + term)
     print("place:" + loc)
-    # term = term
-    # loc = place
     first= request.args.get('one')
     second= request.args.get('two')
     third= request.args.get('three')
@@ -152,13 +106,7 @@ def search_result(term,place):
     ret = sample.query_api(term, place)
     y = json.loads(ret)
     t = list(y.values())
-    # amount = y.slice(-1);
-    # amount = sorted(y.keys())[-4]
-    # amount = sorted(y.keys())
     amount = t[-1]
-    # for p in range(int(amount)):
-    #     lats[p] = ""
-    #     longs[p] = ""
     lats =["" for p in range(int(amount))]
     longs =["" for p in range(int(amount))]
     for a in range(int(amount)):
@@ -168,29 +116,6 @@ def search_result(term,place):
     b = y['2']['coordinates']['longitude']
 
     return render_template('index.html',amount = amount,ret = ret, a= a, b= b, lats= lats,longs=longs)
-# def search_result():
-#     ret = sample.query_api("turkish","New York")
-#     y = json.loads(ret)
-#     t = list(y.values())
-#     # amount = y.slice(-1);
-#     # amount = sorted(y.keys())[-4]
-#     # amount = sorted(y.keys())
-#     amount = t[-1]
-#     # for p in range(int(amount)):
-#     #     lats[p] = ""
-#     #     longs[p] = ""
-#     lats =["" for p in range(int(amount))]
-#     longs =["" for p in range(int(amount))]
-#     for a in range(int(amount)):
-#         lats[a] = y[str(a)]["coordinates"]['latitude']
-#         longs[a] = y[str(a)]["coordinates"]['longitude']
-#     a = y['2']["coordinates"]['latitude']
-#     b = y['2']['coordinates']['longitude']
-#
-#     return render_template('index.html',amount = amount,ret = ret, a= a, b= b, lats= lats,longs=longs)
-
-    # return render_template('index.html', ret = ret)
-
 
 @app.route('/search', methods=["GET"])
 def search():
@@ -202,31 +127,6 @@ def search():
         term = "indian"
     price = "1,2,3,4"
     return redirect(url_for('welcome',term=term,place=loc, price=price))
-
-# @app.route('/searchp', methods=["GET"])
-# def searchp():
-#     term = request.args.get('term')
-#     loc = request.args.get('place')
-#     prices = requests.args.get('price')
-#     if(loc == ""):
-#         loc = "new york"
-#     if(term == ""):
-#         term = "indian"
-#     # if(price =)
-#     return redirect(url_for('welcome',term=term,place=loc))
-
-# @app.route('/search', methods=["GET"])
-# def search():
-#     '''If search query is for books, then redirects to book_search.
-#     If search query is for books, then redirects to movie_search.'''
-#     place = request.args.get("h")
-#     query =request.args.get("query").replace(" " , "+")
-#     if(query==""):
-#         return redirect(url_for('index'))
-#     # if(type == "Books"):
-#     #     return redirect(url_for('book_search', query=query))
-#     # else:
-#     #     return redirect(url_for('movie_search', query=query))
 
 
 if __name__ == '__main__':
